8f177bff9709e93486f918272e31a15437583c0a
Shami Tomita <shamitomita@gmail.com>
2021-01-15 16:43:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
-  min_value = 0
-  name_hash.collect do |key, value|
+  first_value = 0
+  name_hash.each do |key, value|
+    
     
       
     
