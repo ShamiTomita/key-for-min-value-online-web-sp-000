707480553e832ce3b1821dd59d60627be8115ecd
Shami Tomita <shamitomita@gmail.com>
2021-01-15 16:28:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,14 +3,4 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    array.push(value)
-  end 
-  array.each do |a, b|
-    if a < b 
-      array.reject(a)
-    if a > b 
-      array.reject(b)
-  end 
-end 
-end 
-end
+    
