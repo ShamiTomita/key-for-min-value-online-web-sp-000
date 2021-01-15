f20a0c7cdd7b24b50f42d27d50ab31e8c151e841
Shami Tomita <shamitomita@gmail.com>
2021-01-15 16:36:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,7 @@
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(name_hash)
   min_value = 0
-  name_hash.each do |key, value|
-    if value > min_value 
-      min_value == value 
-    
+  name_hash.collect do |key, value|
+    value
       
     
