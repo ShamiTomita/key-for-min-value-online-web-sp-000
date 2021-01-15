b6cf731bbb40461895258cd07a975e805271315d
Shami Tomita <shamitomita@gmail.com>
2021-01-15 16:21:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-array = []
-name_hash.each do |key, value|
-  array.push(value)
+  array = []
+  name_hash.each do |key, value|
+    array.push(value)
+  end 
+  array.each do |a, b|
 end
